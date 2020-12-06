00d69fba6c3db43ef82ee59aaddc4ae407eb09e6
codymerritt347 <cody.merritt@me.com>
2020-12-06 18:44:35 -0500

    created version.rb, updated enviornment.rb

---
+++ lib/environment.rb
@@ -0,0 +1,7 @@
+require_relative './quotebook/api'
+require_relative './quotebook/cli'
+require_relative './quotebook/quote'
+require_relative './quotebook/version'
+
+require 'rake'
+require 'httparty'
---
+++ lib/quotebook/version.rb
@@ -0,0 +1,3 @@
+module Quotebook
+  VERSION = "0.0.1"
+end
