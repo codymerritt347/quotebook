4663ead1de33e73803704ef91ea220f109e42997
codymerritt347 <cody.merritt@me.com>
2020-12-06 22:06:28 -0500

    created #initialize in Quote class

--- lib/quotebook/quote.rb
+++ lib/quotebook/quote.rb
@@ -1,5 +1,13 @@
 class Quote
   @@all = []
+  attr_accessor :text, :author, :favorite
+
+  def initialize(text:, author:, favorite:)
+    self.text = text
+    self.author = author
+    self.favorite = false
+    save
+  end
 
   def save
     self.class.all << self
