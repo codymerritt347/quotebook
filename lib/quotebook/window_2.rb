ed6d557482fe624eaa195bdf17a68dc8dce2ee9c
codymerritt347 <cody.merritt@icloud.com>
2020-12-15 12:23:01 -0500

    cleaning up code smell

--- lib/quotebook/window_2.rb
+++ lib/quotebook/window_2.rb
@@ -1,10 +1,12 @@
+module Screen
+   def self.clear
+       print "\e[2J\e[f"
+   end
+end
+
 class Window_2
 
-   @@return_data = []
-
-   def self.return_data
-      @@return_data
-   end
+   include Screen
 
    # Window Parts
 
@@ -20,6 +22,7 @@ class Window_2
          content
       end
       puts box
+      content
    end
 
    def self.bottom_bar(message = "")
@@ -29,37 +32,74 @@ class Window_2
       puts box
    end
 
+   def self.options_bar(*options)
+      options_string = ""
+      options.each_with_index do |value, index|
+         options_string += "#{index+1}. #{value} // "
+      end
+      box = TTY::Box.frame(width: 60, height: 4) do
+         options_string
+      end
+      puts box
+      options.count
+   end
+
    # Common Windows
 
+   def self.alert_window(message)
+      Screen.clear
+      top_bar("ALERT!")
+      middle_section(message)
+      bottom_bar
+   end
+
    def self.quote_window(quote)
-      top_bar("QUOTE:")
+      Screen.clear
+      top_bar("QUOTE")
       middle_section(
          "#{quote.text}\n-#{quote.author.name}"
       )
       bottom_bar("From: #{quote.genre.name}")
    end
 
-   def self.alert_window(message)
-   end
-
    # CLI Windows
 
-   def self.welcome_window
-   end
-
    def self.main_menu_window
+      Screen.clear
+      top_bar("MAIN MENU")
+      middle_section(" [TO DO:] create welcome ")
+      options_bar("RANDOM", "TOPICS", ["MY FAVORITES"])
    end
 
    def self.randomizer_window
+      Screen.clear
+      random_num = rand(0..119)
+      quote_window(Quote.all[random_num])
+      Quote.all[random_num]
+      options_bar("ANOTHER!", "FAVE", "MAIN")
    end
 
    def self.topics_window
+      Screen.clear
+      top_bar("TOPICS")
+      middle_section(
+
+      )
    end
 
    def self.one_topic_window(topic)
+      top_bar("#{topic.name}")
+      middle_section(
+
+      )
    end
 
    def self.my_favorites_window
+      top_bar("MY FAVORITES")
+      middle_section(
+
+      )
+      options_bar
    end
 
    def self.exit_window
